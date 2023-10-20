36759d18a6544a9313dc80411c4b2b64b39b4533
888-coding <pachelee@gmail.com>
2023-10-20 10:54:43 -0300

    Updated view.py

--- view.py
+++ view.py
@@ -4,3 +4,9 @@ import sqlite3 as lite
 # Creating connection
 con = lite.connect("database")
 
+# Inserting data to database
+with con:
+    dados = []
+    cur = con.cursor()
+    query = "INSERT INTO inventario(nome, local, descricao, marca, data_da_compra, valor_da_compra,serie, imagem) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+    cur.execute(query, dados)
