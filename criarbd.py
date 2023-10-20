0a2c9e05fc9b73a8c4200288b8643f9093da2188
888-coding <pachelee@gmail.com>
2023-10-20 10:23:12 -0300

    Created the database.db & update criarbd.py

--- criarbd.py
+++ criarbd.py
@@ -1,7 +1,7 @@
 import sqlite3 as lite
 con = lite.connect("database.db")
 with con:
-    cur = con.execute()
+    cur = con.cursor()
     query = "CREATE TABLE inventario (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, local TEXT, descricao TEXT, marca TEXT, data_da_compra DATE, valor_da_compra DECIMAL, serie TEXT, imagem TEXT)"
     cur.execute(query)
 
