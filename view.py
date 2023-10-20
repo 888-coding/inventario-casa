a1977cd90e81ade1d4164bec8369eae98adaa301
888-coding <pachelee@gmail.com>
2023-10-20 11:00:13 -0300

    Inserting data to database (is working)

--- view.py
+++ view.py
@@ -2,11 +2,11 @@
 import sqlite3 as lite
 
 # Creating connection
-con = lite.connect("database")
+con = lite.connect("database.db")
 
 # Inserting data to database
 with con:
-    dados = []
+    dados = ['sofa', 'sala de estar', 'sofa comprado em loja casas bahia', 'Marca X', '28/05/1997', 600.0, 'xxxxxxxxx', 'c:imagem']
     cur = con.cursor()
     query = "INSERT INTO inventario(nome, local, descricao, marca, data_da_compra, valor_da_compra,serie, imagem) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
     cur.execute(query, dados)
