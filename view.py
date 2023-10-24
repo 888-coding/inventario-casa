29ff971a104b339ebb03d65dfabe527153339a28
888-coding <pachelee@gmail.com>
2023-10-24 10:18:07 -0300

    Criacao funcao para insert

--- view.py
+++ view.py
@@ -5,8 +5,10 @@ import sqlite3 as lite
 con = lite.connect("database.db")
 
 # Inserting data to database
-with con:
-    dados = ['sofa', 'sala de estar', 'sofa comprado em loja casas bahia', 'Marca X', '28/05/1997', 600.0, 'xxxxxxxxx', 'c:imagem']
-    cur = con.cursor()
-    query = "INSERT INTO inventario(nome, local, descricao, marca, data_da_compra, valor_da_compra,serie, imagem) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-    cur.execute(query, dados)
+def inserir_dados()
+    with con:
+        dados = ['armario', 'quarto', 'comprado na casas bahia', 'Marca Y', '31/05/2000', 1600.0, 'xxxxxxxxx', 'c:imagem']
+        cur = con.cursor()
+        query = "INSERT INTO inventario(nome, local, descricao, marca, data_da_compra, valor_da_compra,serie, imagem) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+        cur.execute(query, dados)
+
