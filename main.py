a5c67c9095da36686b99a43979749d65e8dd4edd
888-coding <pachelee@gmail.com>
2023-11-04 08:21:32 -0300

    Comecando a mexer com grafica com biblioteca Tkinter

--- main.py
+++ main.py
@@ -5,6 +5,20 @@
 from tkinter import *
 from tkinter import Tk, StringVar, ttk
 
+# Cores
+co9 = "#e9edf5"
+
 # Criando Janela
 janela = Tk()
 janela.title('')
+janela.geometry('900x600')
+janela.configure(background=co9)
+janela.resizable(width=FALSE, height=FALSE)
+
+style = ttk.Style()
+style.theme_use("clam")
+
+# Criando frames
+
+
+janela.mainloop()
