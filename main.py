2e7a8ba3aa73fef252e43a8d90f2df3cac42446c
888-coding <pachelee@gmail.com>
2023-11-10 09:43:59 -0300

    No frame Cima, adicionado imagem e texto, com biblioteca pillow

--- main.py
+++ main.py
@@ -5,9 +5,19 @@
 from tkinter import *
 from tkinter import Tk, StringVar, ttk
 
+from PIL import Image, ImageTk
+
 # Cores
-co1 = "#ffffff"
-co9 = "#e9edf5"
+co0 = "#2e2d2b" # preta
+co1 = "#feffff" # branca
+co2 = "#4fa882" # verde
+co3 = "#38576b" # valor
+co4 = "#403d3d" # letra
+co5 = "#e06636" # - profit
+co6 = "#038cfc" #azul
+co7 = "#3fbfb9" # verde
+co8 = "#263238" # + verde
+co9 = "#e9edf5" # - verde
 
 # Criando Janela
 janela = Tk()
@@ -29,4 +39,14 @@ frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
 
 frameBaixo = Frame(janela, width=1043, height=300, bg=co1, relief=FLAT)
 frameBaixo.grid(row=2, column=0, pady=1, padx=0, stick=NSEW)
+
+# Imagem
+app_img = Image.open('inventario.png')
+app_img = app_img.resize((45,45))
+app_img = ImageTk.PhotoImage(app_img)
+
+app_logo = Label(frameCima, image=app_img, text=' Inventário Doméstico', width=900, compound=LEFT, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
+app_logo.place(x=0, y=0)
+
+
 janela.mainloop()
