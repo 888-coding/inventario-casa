ccdd1d35fd63f4e0faea57dff0b86b4eaa04963a
888-coding <pachelee@gmail.com>
2023-11-10 00:08:45 -0300

    Criando frames: Cima, Meio, Baixo

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter import *
 from tkinter import Tk, StringVar, ttk
 
 # Cores
+co1 = "#ffffff"
 co9 = "#e9edf5"
 
 # Criando Janela
@@ -20,5 +21,12 @@ style.theme_use("clam")
 
 # Criando frames
 
+frameCima = Frame(janela, width=1043, height=50, bg=co1, relief=FLAT)
+frameCima.grid(row=0, column=0)
 
+frameMeio = Frame(janela, width=1043, height=303, bg=co1, pady=20, relief=FLAT)
+frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+
+frameBaixo = Frame(janela, width=1043, height=300, bg=co1, relief=FLAT)
+frameBaixo.grid(row=2, column=0, pady=1, padx=0, stick=NSEW)
 janela.mainloop()
