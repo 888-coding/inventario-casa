82878cc05b86f62adbf5e9b92b2178571ad832bd
888-coding <pachelee@gmail.com>
2023-11-11 19:55:58 -0300

    Adicionado mais Labels e Entradas para frame Meio

--- main.py
+++ main.py
@@ -50,11 +50,27 @@ app_logo = Label(frameCima, image=app_img, text=' Inventário Doméstico', width
 app_logo.place(x=0, y=0)
 
 # Trabalhando no frame Meio ============================================
+
+#  Criando entradas
 l_nome = Label(frameMeio, text='Nome', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_nome.place(x=10, y=10)
-
 e_nome = Entry(frameMeio, width=30, justify='left', relief=SOLID)
 e_nome.place(x=130,y=11)
+
+l_local = Label(frameMeio, text='Sala/Área', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_local.place(x=10, y=40)
+e_local = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_local.place(x=130,y=41)
+
+l_descricao = Label(frameMeio, text='Descrição', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_descricao.place(x=10, y=70)
+e_descricao = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_descricao.place(x=130,y=71)
+
+l_modelo = Label(frameMeio, text='Marca/Modelo', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_modelo.place(x=10, y=100)
+e_modelo = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_modelo.place(x=130,y=101)
 
 
 janela.mainloop()
